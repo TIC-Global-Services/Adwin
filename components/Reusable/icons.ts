8b3b2075f8825c1545de13682a20ca0c9e8215aa
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-07-18 16:48:53 +0530

    Overview Images Bug

--- components/Reusable/icons.ts
+++ components/Reusable/icons.ts
@@ -69,11 +69,11 @@ export {default as Company} from '@/assets/About/Companies/Frame 1171275306.png'
 export {default as MobileCompany} from '@/assets/About/Companies/mobile_BgIMage.png';
 
 //about - directories
-export {default as Director1} from '@/assets/About/Companies/dir1.png';
-export {default as Director2} from '@/assets/About/Companies/dir2.png';
-export {default as Director3} from '@/assets/About/Companies/dir3.png';
-export {default as Director4} from '@/assets/About/Companies/dir4.png';
-export {default as Director5} from '@/assets/About/Companies/dir5.png';
+export {default as Director1} from '@/assets/About/companies/dir1.png';
+export {default as Director2} from '@/assets/About/companies/dir2.png';
+export {default as Director3} from '@/assets/About/companies/dir3.png';
+export {default as Director4} from '@/assets/About/companies/dir4.png';
+export {default as Director5} from '@/assets/About/companies/dir5.png';
 
 
 // About - sustainability
